b385e2e4fee40f4c92c8d58d26142c93454ffebe
Ziad Hegazi <ziadhegazi@hotmail.com>
2025-05-25 16:31:54 +0300

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -53,5 +54,13 @@ def evaluate_models(x_train, y_train, x_test, y_test, models, params):
 
             report[list(models.keys())[i]] = [train_model_score, test_model_score]
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+
+####### Loading object files by taking the filepath we want to load it from
+def load_object(filepath):
+    try:
+        with open(filepath, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
